d65e5cd367aa3c5b487bf8f691fb1e034019e312
Retiks <100346999+Retiks@users.noreply.github.com>
2022-03-10 10:19:09 +0100

    Add files via upload

---
+++ connexion2/connexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page de Connexion</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div id="container">
+      <form action="bdd.php" method="POST">
+        <h1>Connexion</h1>       
+        <label><b>Nom d'utilisateur : </b></label>
+        <input type="text" id="mon_input" placeholder="Entrer le nom d'utilisateur" name="username" required>
+        <br>
+        <br>
+        <label><b>Mot de passe : </b></label>
+        <input type="password" id="mon_input2" placeholder="Entrer le mot de passe" name="password" required>
+        <br>
+        <br>
+        <input type="submit" id='submit' value='Login' >
+        <?php
+        if(isset($_GET['erreur'])){
+          $err = $_GET['erreur'];
+          if($err==1 || $err==2)
+            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+        ?>
+      </form>
+    </div>
+  </body>
+</html>
